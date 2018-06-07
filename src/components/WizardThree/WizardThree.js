8c319accac15dd27444258e7360c839ca5d6ad02
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-06-06 20:59:31 -0600

    finished project

--- src/components/WizardThree/WizardThree.js
+++ src/components/WizardThree/WizardThree.js
@@ -5,6 +5,7 @@ import {updateProp} from '../../ducks/reducer.js';
 
 class WizardThree extends Component {
     render(){
+        const {updateProp}=this.props;
         return(
             <div className="parent-div">
                 <div className="vert-align">
